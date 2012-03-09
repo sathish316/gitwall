db0fe62b1960fe4c8d6ced3f88a27990697eec89
Sathish <sathish316@gmail.com>
2012-03-10 01:26:49 +0530

    Add one task to session and display

--- src/tasks/core.clj
+++ src/tasks/core.clj
@@ -9,15 +9,29 @@
 
 (defn add-new-task-form []
   (html
-   (form-to [:post "/tasks/create"]
+   (form-to [:post "/tasks"]
             "Task"
             [:br]
             (text-field :task)
             (submit-button "Add"))))
 
+(defn create-task [task]
+  {:body (str "Saved " task)
+   :session {:tasks task}})
+
+(defn view-tasks [tasks]
+  (html
+   [:h1 (str "Tasks")]
+   [:p tasks]))
+
 (defroutes myroutes
   (GET "/" [] (display))
-  (GET "/tasks/new" [] (add-new-task-form)))
+  (GET "/tasks/new" [] (add-new-task-form))
+  (POST "/tasks" {params :params}
+        (create-task (params :task)))
+  (GET "/tasks" {session :session}
+       (view-tasks (:tasks session))))
+        
 
 (def app
   (handler/site myroutes))
