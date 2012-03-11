d219c41ebcbfff85fdd91cf416d977e3fda6bbaf
Sathish <sathish316@gmail.com>
2012-03-12 03:25:36 +0530

    mongo port config

--- src/tasks/mongo.clj
+++ src/tasks/mongo.clj
@@ -3,7 +3,7 @@
 
 (defn mongo-params [mongo-uri]
   (let [[all user password host port db] (re-find #"mongodb://(\w+):(\w+)@(.*?):(\w+)\/(\w+)" mongo-uri)]
-    {:user user :password password :host host :port port :db db}))
+    {:user user :password password :host host :port (Integer. port) :db db}))
 
 (defn connect! []
   (if (System/getenv "MONGOLAB_URI")
