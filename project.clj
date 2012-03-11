d439fb4034b3b3600e8ede801309595e0eb8d653
Sathish <sathish316@gmail.com>
2012-03-12 03:00:26 +0530

    Adding ring jetty adapter

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [clj-oauth2 "0.2.0"]
                  [clj-http "0.3.3"]
                  [org.clojure/data.json "0.1.2"]
-                 [congomongo "0.1.8"]]
+                 [congomongo "0.1.8"]
+                 [ring/ring-jetty-adapter "1.1.0-SNAPSHOT"]]
   :dev-dependencies [[lein-ring "0.5.4" :exclusions [hiccup]]]
   :ring {:handler tasks.web/app})
