e1654728c2323c336042e3e9f64e85e0e09434bd
Sathish <sathish316@gmail.com>
2012-03-12 03:22:47 +0530

    mongo config for dev and prod

--- src/tasks/mongo.clj
+++ src/tasks/mongo.clj
@@ -1,20 +1,23 @@
 (ns tasks.mongo
   (:use somnium.congomongo))
 
-(defn make-connection-for-env [env]
-  (condp = env
-    :dev
-    (make-connection "gitwall"
-                     :host "127.0.0.1"
-                     :port 27017)
-    :prod
-    (make-connection "gitwall"
-                     :host "127.0.0.1"
-                     :port 27017)))
-
-(def conn (make-connection-for-env :dev))
+(defn mongo-params [mongo-uri]
+  (let [[all user password host port db] (re-find #"mongodb://(\w+):(\w+)@(.*?):(\w+)\/(\w+)" mongo-uri)]
+    {:user user :password password :host host :port port :db db}))
 
 (defn connect! []
-  (set-connection! conn))
+  (if (System/getenv "MONGOLAB_URI")
+    (let [params (mongo-params (System/getenv "MONGOLAB_URI"))
+          connection (make-connection (:db params)
+                                      :host (:host params)
+                                      :port (:port params))]
+      (set-connection! connection)
+      (authenticate connection
+                    (:user params)
+                    (:password params)))
+    (let [connection (make-connection "gitwall"
+                                      :host "127.0.0.1"
+                                      :port 27017)]
+      (set-connection! connection))))
 
 (connect!)
