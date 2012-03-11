01cfd7575f19d28b5f342d937e0bae752411d9c3
Sathish <sathish316@gmail.com>
2012-03-12 03:56:45 +0530

    fixing github callback url

--- src/tasks/oauth_github.clj
+++ src/tasks/oauth_github.clj
@@ -4,9 +4,13 @@
 (def github-oauth2
   {:authorization-uri "https://github.com/login/oauth/authorize"
    :access-token-uri "https://github.com/login/oauth/access_token"
-   :redirect-uri "http://localhost:3000/github_oauth2_callback"
-   :client-id (or (System/getenv "GITHUB_CLIENT_ID") "91dc35fe530ab2a358b2")
-   :client-secret (or (System/getenv "GITHUB_CLIENT_SECRET") "4914f9d2be376e4579e04ef203ebe37af4079f2b")
+   :redirect-uri (if (System/getenv "GITHUB_CLIENT_ID")
+                   "http://gitwall.herokuapp.com/github_oauth2_callback"
+                   "http://localhost:3000/github_oauth2_callback")
+   :client-id (or (System/getenv "GITHUB_CLIENT_ID")
+                  "91dc35fe530ab2a358b2")
+   :client-secret (or (System/getenv "GITHUB_CLIENT_SECRET")
+                      "4914f9d2be376e4579e04ef203ebe37af4079f2b")
    :access-query-param :access-token
    :grant-type "authorization_code"})
 
