ea0117a2428851ad10e7ee36039344b3b3d25deb
Sathish <sathish316@gmail.com>
2012-03-13 08:00:00 +0530

    delete example

--- src/tasks/sample.clj
+++ src/tasks/sample.clj
@@ -10,5 +10,6 @@
    {:id 201 :title "Add New Task" :status 1 :class "sample_task"}
    {:id 202 :title "Sign in to Github to access Projects" :status 1 :class "sample_task"}
    {:id 203 :title "Completed task" :status 3 :class "sample_task"}
+   {:id 204 :title "Task can be deleted using this icon" :status 1 :class "sample_task sample_delete_task" }
    ])
 
